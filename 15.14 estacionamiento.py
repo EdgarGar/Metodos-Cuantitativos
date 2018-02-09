176226dbcf0bd3259ad6af21a3c6d3b7e0c304c2
GARZA800 <luciagarza90@gmail.com>
2018-02-08 23:17:15 -0600

    Update and rename 15.14_estacionamiento.py to 15.14 estacionamiento.py
    YAQUEDOYUPI

--- 15.14 estacionamiento.py
+++ 15.14 estacionamiento.py
@@ -16,9 +16,9 @@ El tiempo que un auto permanece en el estacionamiento sigue una distribucion uni
 import random
 import math
 
-horas = 24
-minutos = 60 
-clientes = 10
+horas = 24 #Declarar numero de horas a simular
+minutos = 60 #Cantidad de minutos por 10 clientes
+clientes = 10 #Cantidad de clientes por 60 minutos
 coche_perdido = coche_estacionado = estacionado = disponibles = 0
     
 for hora in range(horas):
@@ -29,7 +29,6 @@ for hora in range(horas):
         minuto_llegada = random.randint(1,60)
         llegadas.append(minuto_llegada)
     llegadas.sort() #Ordenar los coches 
-    #print llegadas
     for i in range (clientes):
         tiempo = random.randint(10,30)
         salidas.append(tiempo + llegadas[i])
@@ -38,21 +37,25 @@ for hora in range(horas):
         for coche in range (clientes):
             if(llegadas[coche] == i):
                 if(estacionamiento > 0):
-                    #print "Coche estacionado"
+                    #Coche estacionado
                     coche_estacionado += 1
                     estacionamiento -= 1
                 else:
-                    #print "Coche perdido"
+                    #Coche perdido
                     coche_perdido += 1
             if(salidas[coche] == i):
-                #print "Coche salio"
+                #Coche salio
                 estacionamiento +=  1
-
+                
+    disponibles += estacionamiento #Lugares disponibles 
+    
 promedio_estacionado = float(coche_estacionado)/(clientes*horas)
 promedio_perdido = float(coche_perdido)/(clientes*horas)
+promedio_disponible = float(disponibles)/(6*horas)
 
 print "Estacionamiento: {}".format(estacionamiento)
 print "Coches que encontraron estacionamiento: {} de {} ".format(coche_estacionado, clientes)
 print "Coches que se fueron por estacionamiento lleno: {} de {} ".format(coche_perdido, clientes)
-print "Probabilidad de estacionar un coche en {} horas: {}".format(horas,promedio_estacionado)
-print "Probabilidad de perder un coche en {} horas: {}".format(horas,promedio_perdido)
+print "Probabilidad de estacionar un coche en {} horas: {}".format(horas,round(promedio_estacionado,3))
+print "Probabilidad de perder un coche en {} horas: {}".format(horas,round(promedio_perdido,3))
+print "Porcentaje de lugares disponibles: {}%".format(round(promedio_disponible*100, 2))
